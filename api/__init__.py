7bd4a010c14becd80af76495136f3beeaf641eac
Piyumal98 <59592735+Piyumal98@users.noreply.github.com>
2022-10-05 06:35:00 +0530

    2nd commit

---
+++ api/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from firebase_admin import credentials, initialize_app
+
+cred = credentials.Certificate("api/Key.json")
+default_app = initialize_app(cred)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '123pyflfb'
+
+    from .userAPI import userAPI
+
+    app.register_blueprint(userAPI, url_prefix='/user')
+
+    return app 
