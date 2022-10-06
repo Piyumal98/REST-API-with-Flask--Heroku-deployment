7de812251a61d537033a6d85926ba3cfa970ec6f
Piyumal98 <59592735+Piyumal98@users.noreply.github.com>
2022-10-07 03:37:00 +0530

    23

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from api import create_app 
 
 app = create_app()
 
-if __name__ =='__main__':
-    app.run(debug = True)
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
